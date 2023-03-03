5322376a01e00a543866cad85d1cbb1b76da4357
EEDAT <EEDAT@TEB114104>
2023-03-03 15:03:17 +0100

    Add project files.

---
+++ uppgift 6.1 khamzat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppgift_61_Khamzat
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int summa = Addition(1, 2, 3);
+            Console.WriteLine("Summan är:" + summa);
+        }
+        static int Addition(int tal1, int tal2, int tal3)
+        {
+
+            return (tal1 + tal2 + tal3);
+
+
+        }
+    }
+
+}   //klart
